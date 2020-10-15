bfc7393db3c413353482b56e95a80f6e5d72fdac
jork41989 <jork41989@gmail.com>
2020-10-15 17:10:42 -0400

    removed extra code for old form

--- src/components/webinar_form/webinarForm.jsx
+++ src/components/webinar_form/webinarForm.jsx
@@ -1,8 +1,5 @@
-import React, { useState } from "react";
-import isValidZipcode from "is-valid-zipcode"
-import WebinarFormFeilds from './webinarFromFields'
+import React from "react";
 
-import * as EmailValidator from 'email-validator';
 
 import './form.css'
 
@@ -16,127 +13,6 @@ import './form.css'
 
 export default function WebinarForm() {
 
-  const [formData, setFormData] = useState({firstName:"", lastName: "", email:"", phone: "", address:"", city:"", state:"", zipcode:""})
-  const [formErrors, setFormErrors] = useState({ firstName: false, lastName: false, email: false, phone: false, address: false, city: false, state: false, zipcode: false })
-  const [badData, setBadData] = useState()
-
-  let changer = (e) =>{
- 
-    let type = e.target.id
-    let val = e.target.value
-    let oldData = formData
-    if(type === "zipcode"){
-      console.log(isValidZipcode(val))
-      console.log(document.getElementById("zipcode"))
-    } else if (type === "email"){
-      console.log(EmailValidator.validate(val))
-    }
-    oldData[type] = val
-    setFormData(oldData)
-    console.log(formData)
-  }
-  
-  let stateAbr = (e) =>{
-
-    let val = e.target.value
-    let oldData = formData
-    oldData.state = val
-    setFormData(oldData)
-    console.log(formData)
-  }
-
-  let submitter = async (e) =>{
-    e.preventDefault()
-    console.log("submitting")
-    
-    
-    await checks(e)
-      .then(
-
-        sendData(formData)
-      )
-    // const params = {
-    //   spreadsheetId: SPREADSHEET_ID,
-    //   range: 'Sheet1',
-    //   valueInputOption: 'RAW',
-    //   insertDataOptions: 'INSERT_ROWS'
-    // }
-    // const valueRangeBody = {
-    //   'majorDimension': 'ROWS',
-    //   'values': [Object.values(formData)]
-    // }
-    // console.log(badData)
-    // if(!badData){
-    //   // let request = gapi.client.sheets.spreadsheets.values.append(params, valueRangeBody)
-    //   // try{
-    //   //   let res = request
-    //   //   console.log(res)
-    //   // } catch(err){
-    //   //   console.log(err)
-    //   // }
-    //   sendData(Object.values(formData))
-    // }
-
-      
-  } 
-
-  let checks = async (e) =>{
-    e.preventDefault()
-    let oldErrors = formErrors
-    setBadData(false)
-    if (formData.state === '') {
-      setBadData(true)
-      oldErrors.state = true
-      setFormErrors(oldErrors)
-      console.log("state")
-    }
-    if (!isValidZipcode(formData.zipcode)) {
-      setBadData(true)
-      oldErrors.zipcode = true
-      setFormErrors(oldErrors)
-      console.log("zip")
-    }
-
-    if (!EmailValidator.validate(formData.email)) {
-      setBadData(true)
-      oldErrors.email = true
-      setFormErrors(oldErrors)
-      console.log("email")
-    }
-    console.log(badData)
-  }
-
-  let sendData = async (formInfo) => {
- 
-
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbyCxRkwxX7TFY7tF7En6IaAtbgRKShtbE7MZ91MF6f_JSO1LrDt/exec'
-    // console.log("testing")
-  
-
-    fetch(scriptURL, { method: 'POST', body: formData, mode: "no-cors", })
-        .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message))
-    
-
-
-  }
-
-
-  // let handleClientLoad = () =>{
-  //     gapi.load('client:auth2', initClient)
-  // }
-
-  // let initClient = () => {
-  //   gapi.client.init({
-  //     'apiKey': API_KEY,
-  //     'clientId': CLIENT_ID,
-  //     'scope': SCOPE,
-  //     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']
-  //   }).then(()=>{
-  //     gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSignInStatus);
-  //     this.updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  //   })
-  // }
 
 
 
@@ -144,7 +20,7 @@ export default function WebinarForm() {
     <div className="contentMain backg3" id="form_body">
       <div className="formBack">
         {/* <WebinarFormFeilds changer={changer} stateAbr={stateAbr} formErrors={formErrors} submitter={submitter} /> */}
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSctsyMUl0sFceVWJjzOC5uvz8rrrQnBI13UjMji7bMNNFUBbQ/viewform?embedded=true" className="formFrame">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSctsyMUl0sFceVWJjzOC5uvz8rrrQnBI13UjMji7bMNNFUBbQ/viewform?embedded=true" className="formFrame" title="PPAForm">Loading…</iframe>
       </div>
 
      </div>
